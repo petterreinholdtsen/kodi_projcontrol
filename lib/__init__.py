c150f9a6fe20a1c97e4ae2c09bd2707be67fa1fe
Petter Reinholdtsen <pere@hungry.com>
2018-06-25 08:42:06 +0200

    Included embedded copy of pyserial.

--- lib/__init__.py
+++ lib/__init__.py
@@ -2,6 +2,10 @@
 # Copyright (c) 2015 Fredrik Eriksson <git@wb9.se>
 # This file is covered by the BSD-3-Clause license, read LICENSE for details.
 
+# Add lib/ext/ to Python search path to find embedded pyserial.
+import os, sys
+sys.path.append(os.path.join(os.path.dirname(__file__), 'ext'))
+
 CMD_PWR_ON="poweron"
 CMD_PWR_OFF="poweroff"
 CMD_PWR_QUERY="powerquery"
